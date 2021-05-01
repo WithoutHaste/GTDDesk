05cfb84fc96bc50d643b265d01a68cbee7c5ae84
J M <wohaste@gmail.com>
2021-04-30 19:00:06 -0600

    load settings from file

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# GTDDesk
+# GTD Desk
+
 Windows app for organizing "Getting Things Done" projects
+
+## CLI
+
+Project is starting as a console app aka command line interface.
+
+Run `GTDDesk_CLI.exe` to start.
+
+## Configuration
+
+Add a text file called `appsettings.json` in the directory where the `exe` lives.
+
+Template:
+```
+{
+	"Directory": "full_path_to_directory_containing_project_files"
+}
+```
--- codebase/GTDDesk_CLI/Program.cs
+++ codebase/GTDDesk_CLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 using GTDDesk_Core;
 
 namespace GTDDesk_CLI
@@ -12,6 +13,18 @@ namespace GTDDesk_CLI
             {
                 Console.WriteLine(project);
             }
+
+            IConfiguration config = GetConfig();
+            Console.WriteLine(config["Directory"]);
+        }
+
+        private static IConfiguration GetConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(System.AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
         }
     }
 }
